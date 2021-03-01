51d390ce1e96acf406d2a46ee83b9f7309655631
Imre Toth <tothimre@gmail.com>
2021-03-02 00:52:53 +0100

    .

--- tests/tests/unit-tests-linker-file.js
+++ tests/tests/unit-tests-linker-file.js
@@ -1,9 +1,5 @@
 /* eslint-env mocha */
-// require('../../../../lib/test-common')
 const path = require('path')
-// const fs = require('fs')
-// const copyFileSync = require('fs-copy-file-sync')
-// const appContainer = require(`../../../../../src/app/container`)()
 const readmeFileName = 'README.md'
 
 const cwd = require('pkg-dir').sync(__dirname)
